71e75ca604a332b88d3e2bb77cd4cf142dccd059
iked <hongsangdk97@gmail.com>
2019-06-16 11:08:27 +0700

    update

--- PuzzleGame/app/src/main/java/com/t3h/puzzlegame/MainActivity.java
+++ PuzzleGame/app/src/main/java/com/t3h/puzzlegame/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton btnPlay;
+    EditText edtPlayerName;
     public static final int REQUEST_PLAY =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView() {
         btnPlay = findViewById(R.id.btnPlay);
+        edtPlayerName = findViewById(R.id.edtPlayerName);
 
         btnPlay.setOnClickListener(this);
 
@@ -28,7 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(this,PlayActivity.class);
-        startActivityForResult(intent,REQUEST_PLAY);
+        String playerName = edtPlayerName.getText().toString();
+        if (playerName.isEmpty()){
+            Toast.makeText(this,"Enter your name",Toast.LENGTH_SHORT).show();
+        }else {
+            Intent intent = new Intent(this,PlayActivity.class);
+//            intent.putExtra()
+            startActivityForResult(intent,REQUEST_PLAY);
+        }
     }
 }
